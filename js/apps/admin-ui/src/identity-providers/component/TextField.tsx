c3e9afb061f49b283adee39517d4a97c715546b0
Robert Chan <robert@devinium.com>
2025-02-19 18:18:20 +1100

    Updated the TextField to style like a disabled field when it is read only

--- js/apps/admin-ui/src/identity-providers/component/TextField.tsx
+++ js/apps/admin-ui/src/identity-providers/component/TextField.tsx
@@ -5,12 +5,18 @@ import { FieldProps, FormGroupField } from "./FormGroupField";
 
 export const TextField = ({ label, field, isReadOnly = false }: FieldProps) => {
   const { register } = useFormContext();
+
+  const style = isReadOnly
+    ? { backgroundColor: "#444548", cursor: "not-allowed", color: "#AAB0AC" }
+    : {};
+
   return (
     <FormGroupField label={label}>
       <TextInput
         id={label}
         data-testid={label}
         readOnly={isReadOnly}
+        style={style} // Apply the custom styles
         {...register(field)}
       />
     </FormGroupField>
